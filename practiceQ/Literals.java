76e93c44c021c84f32d429681a31cc4ccd5bb61f
Rajsahu2006 <rajsahu062006@gmail.com>
2025-11-26 16:04:33 +0530

    uploaded  java file

---
+++ practiceQ/Literals.java
@@ -0,0 +1,17 @@
+package practiceQ;
+
+public class Literals {
+    public static void main(String[] args){
+        byte age = 112;
+        int age2 = 56;
+        char ch = '$';
+        float f1 = 10.7f;
+        boolean a = true;
+        System.out.println(age2);
+        String str = "RAJ";
+         System.out.println(str);
+
+
+    }
+    
+}
---
+++ practiceQ/iner2.java
@@ -0,0 +1,26 @@
+package practiceQ;
+
+class Device {
+    void PowerOn(){
+        System.out.println("Start a laptop");
+    }
+    
+}
+class  Laptop extends Device {
+    void  boot(){
+        System.out.println("Connicting a device ");
+    }
+}
+class GamingLaptop extends Laptop{
+    void startgame(){
+        System.out.println("start gaming");
+    }
+}
+public class iner2 {
+    public static void main(String[] args) {
+    GamingLaptop g = new GamingLaptop();
+    g.PowerOn
+    g.boot();
+    g.startgame();
+    }
+}
